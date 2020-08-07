c00192833cff9b49317943c11c3889f803bf891e
Brian2524 <b2hinkle@gmail.com>
2020-08-07 05:30:57 -0400

    Add GAS system for health, using meta attributes instead of directly modifying Health

--- Source/SonicSiege/Public/Character/AS_Character.h
+++ Source/SonicSiege/Public/Character/AS_Character.h
@@ -42,9 +42,36 @@ public:
 	ATTRIBUTE_ACCESSORS(UAS_Character, RunAccelaration)
 #pragma endregion
 
+#pragma region Health Attributes
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Attributes")
+		FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UAS_Character, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Attributes", meta = (HideFromModifiers))	// HideFromModifiers removes it from the GE dropdown menu so you can't accidently modify it
+		FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UAS_Character, Health)
+
+
+	//	'meta' attributes: ie. "What do we do with this incomming Damage or Healing?" Good solution in place of an ExecutionCalculation
+	/** This Damage attribute is just used for applying negative health mods. Its not a 'persistent' attribute. It is a 'meta' attribute (gets set back to 0 after it modifies Health) */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Damage, Category = "Attributes", meta = (HideFromLevelInfos))	// You can't make a GameplayEffect 'powered' by Damage (Its transient)
+		FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS(UAS_Character, Damage)
+
+	/** This Healing attribute is just used for applying positive health mods. Its not a 'persistent' attribute. It is a 'meta' attribute (gets set back to 0 after it modifies Health) */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Healing, Category = "Attributes", meta = (HideFromLevelInfos))	// You can't make a GameplayEffect 'powered' by Damage (Its transient)
+		FGameplayAttributeData Healing;
+	ATTRIBUTE_ACCESSORS(UAS_Character, Healing)
+#pragma endregion
 	
 
 protected:
+	//	Handle applying any modifications to incomming effects (ie. subtracting incomming damage using a shield attribute)
+	virtual bool PreGameplayEffectExecute(struct FGameplayEffectModCallbackData& Data) override;
+	//	Handle using 'meta' attributes for modifying 'persistant' attributes. Such as Damage modifying Health
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+
+
 	//These OnReps exist to make sure the GAS internal representations are synchronized properly during replication
 	UFUNCTION()
 		virtual void OnRep_WalkSpeed(const FGameplayAttributeData& ServerBaseValue);
@@ -54,4 +81,13 @@ protected:
 		virtual void OnRep_RunSpeed(const FGameplayAttributeData& ServerBaseValue);
 	UFUNCTION()
 		virtual void OnRep_RunAccelaration(const FGameplayAttributeData& ServerBaseValue);
+
+	UFUNCTION()
+		virtual void OnRep_Health(const FGameplayAttributeData& ServerBaseValue);
+	UFUNCTION()
+		virtual void OnRep_Damage(const FGameplayAttributeData& ServerBaseValue);
+	UFUNCTION()
+		virtual void OnRep_Healing(const FGameplayAttributeData& ServerBaseValue);
+	UFUNCTION()
+		virtual void OnRep_MaxHealth(const FGameplayAttributeData& ServerBaseValue);
 };

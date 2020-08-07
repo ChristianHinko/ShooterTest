0d6d5e27f4dab0a151e8c39d42e6cff575521804
include-iostream <c4hinkle@gmail.com>
2020-08-07 03:14:05 -0300

    make the GA_Fire ability and make the corresponding ability tag for it

---
+++ Source/SonicSiege/Private/Actor/Item/Weapon/Abilities/GA_Fire.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/Item/Weapon/Abilities/GA_Fire.h"
+#include "AbilitySystemComponent.h"
+#include "SonicSiege/Private/Utilities/LogCategories.h"
+
+
+
+UGA_Fire::UGA_Fire()
+{
+	AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Fire")));
+}
+
+
+bool UGA_Fire::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
+{
+	if (!Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void UGA_Fire::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false);
+		return;
+	}
+	if (!FireEffectTSub)
+	{
+		UE_LOG(LogGameplayAbility, Error, TEXT("Effect TSubclassOf empty in %s so this ability was canceled - please fill out Fire ability blueprint"), *FString(__FUNCTION__));
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, false);
+		return;
+	}
+
+
+	FireEffectActiveHandle = ApplyGameplayEffectToOwner(Handle, ActorInfo, ActivationInfo, FireEffectTSub.GetDefaultObject(), GetAbilityLevel());
+
+
+	EndAbility(Handle, ActorInfo, ActivationInfo, false, false);
+}
+
+void UGA_Fire::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	// Super wraps the whole EndAbility() in IsEndAbilityValid()
+	if (!IsEndAbilityValid(Handle, ActorInfo))
+	{
+		return;
+	}
+	// Make sure we bind our child class' version of EndAbility() instead of using the Super's version of this part
+	if (ScopeLockCount > 0)
+	{
+		WaitingToExecute.Add(FPostLockDelegate::CreateUObject(this, &UGA_Fire::EndAbility, Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled));
+		return;
+	}
+
+
+
+	if (ActorInfo)
+	{
+		if (ActorInfo->AbilitySystemComponent.Get())
+		{
+			ActorInfo->AbilitySystemComponent->RemoveActiveGameplayEffect(FireEffectActiveHandle);
+		}
+		else
+		{
+			UE_LOG(LogGameplayAbility, Error, TEXT("%s() ActorInfo->AbilitySystemComponent.Get() was NULL when trying to remove RunEffectActiveHande"), *FString(__FUNCTION__));
+		}
+	}
+	else
+	{
+		UE_LOG(LogGameplayAbility, Error, TEXT("%s() ActorInfo was NULL when trying to remove RunEffectActiveHande"), *FString(__FUNCTION__));
+	}
+
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}

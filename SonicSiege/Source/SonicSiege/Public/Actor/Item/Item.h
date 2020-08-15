25267174e0f37b844891421f71843804f3ce48d3
include-iostream <c4hinkle@gmail.com>
2020-08-14 21:04:29 -0300

    clean up item info system

--- SonicSiege/Source/SonicSiege/Public/Actor/Item/Item.h
+++ SonicSiege/Source/SonicSiege/Public/Actor/Item/Item.h
@@ -4,7 +4,32 @@
 
 #include "CoreMinimal.h"
 #include "Actor/AbilitySystemActor.h"
+
 #include "Item.generated.h"
+
+
+
+enum class EItemType : uint8
+{
+	TYPE_None,
+	TYPE_Gun,
+	TYPE_Melee,
+	TYPE_
+};
+
+/**
+ *  This should store the info for an Item so we can avoid casting for no reason.
+ */
+USTRUCT()
+struct FItemInfo
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	AItem* Item;
+
+	EItemType ItemType;
+};
 
 /**
  * 

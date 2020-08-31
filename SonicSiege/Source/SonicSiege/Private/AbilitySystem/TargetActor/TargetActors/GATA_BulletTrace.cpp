ee0a10284c40b42f6c6aab64e741dd7d92ea941c
include-iostream <c4hinkle@gmail.com>
2020-08-31 00:49:05 -0300

    fix bullet spread no working

--- SonicSiege/Source/SonicSiege/Private/AbilitySystem/TargetActor/TargetActors/GATA_BulletTrace.cpp
+++ SonicSiege/Source/SonicSiege/Private/AbilitySystem/TargetActor/TargetActors/GATA_BulletTrace.cpp
@@ -43,11 +43,11 @@ void AGATA_BulletTrace::PerformTrace(TArray<FHitResult>& OutHitResults, AActor* 
 		DirWithPlayerController(InSourceActor, Params, TraceStart, AimDir);		//Effective on server and launching client only
 
 		// Calculate new AimDir with random bullet spread offset if needed
-		if (bulletSpread <= SMALL_NUMBER)
+		if (bulletSpread > SMALL_NUMBER)
 		{
 			// create net-safe random seed stream
 			const int16 predKey = OwningAbility->GetCurrentActivationInfo().GetActivationPredictionKey().Current;
-			const int32 randomSeed = predKey - (t * 100000000000000000);	// use the prediction key as a net safe seed and add a crazy t-based large number so that each bullet has its own randomness per fire (the larger the number multiplied to t, the less likely bullets will follow the same pattern per fire)
+			const int32 randomSeed = predKey - (t * 100/*000000000000000*/);	// use the prediction key as a net safe seed and add a crazy t-based large number so that each bullet has its own randomness per fire (the larger the number multiplied to t, the less likely bullets will follow the same pattern per fire)
 			const FRandomStream randomStream = FRandomStream(randomSeed);
 
 			// add random offset to AimDir

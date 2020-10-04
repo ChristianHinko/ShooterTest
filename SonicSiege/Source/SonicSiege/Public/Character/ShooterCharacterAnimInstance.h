154a95182cfce405241d9235904d9158d38aa318
Brian2524 <b2hinkle@gmail.com>
2020-10-04 18:12:34 -0400

    Create base anim instance class and shooter anim instance class

---
+++ SonicSiege/Source/SonicSiege/Public/Character/ShooterCharacterAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/SSAnimInstance.h"
+#include "ShooterCharacterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SONICSIEGE_API UShooterCharacterAnimInstance : public USSAnimInstance
+{
+	GENERATED_BODY()
+	
+};

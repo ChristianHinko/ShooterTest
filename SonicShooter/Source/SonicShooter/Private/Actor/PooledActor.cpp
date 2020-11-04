da1bda60abebba16d7f75778afc66d6904a01968
Brian2524 <b2hinkle@gmail.com>
2020-11-04 02:51:15 -0500

    Create the PooledActor class and paste code. It should be transfered correctly but need to fix one last error

---
+++ SonicShooter/Source/SonicShooter/Private/Actor/PooledActor.cpp
@@ -0,0 +1,192 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor\PooledActor.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+APooledActor::APooledActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void APooledActor::Deactivate()
+{
+	//server only
+	if (!HasAuthority()) { return; }
+	this->DeativateToPool();
+	DeactivationBroadcast();
+}
+
+APooledActor* APooledActor::GetFromPool(UWorld* World, UClass* BulletClass)
+{
+	APooledActor* Pool = Cast<APooledActor>(BulletClass->GetDefaultObject());
+
+	if (Pool)
+	{
+		//find first of correct class;
+		bool CleanupRequired = false;
+
+		int32 FoundIndex = Pool->Pooled.IndexOfByPredicate(
+			[&](auto InItem) {
+				if (InItem.IsValid() && InItem->GetWorld() == World)
+				{
+					return true;
+				}
+				else
+				{
+					CleanupRequired = true;
+					return false;
+				}
+			});
+
+		TWeakObjectPtr<APooledActor> Found = nullptr;
+		if (FoundIndex != INDEX_NONE)
+		{
+			Found = Pool->Pooled[FoundIndex];
+			Pool->Pooled.RemoveAtSwap(FoundIndex);
+		}
+
+		if (CleanupRequired)
+		{
+#ifdef WITH_EDITOR
+			if (Pool->DebugPooling)
+			{
+				GEngine->AddOnScreenDebugMessage(2, 2, FColor::White, TEXT("Invalid reference in pool, cleaning up"));
+			}
+#endif
+			Pool->Pooled.RemoveAll([&](auto InItem) {
+				if (InItem.IsValid() && InItem->GetWorld() == World)
+				{
+					return false;
+				}
+				else
+				{
+					return true;
+				}
+				});
+		}
+
+		return(Found.Get());
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+APooledActor* APooledActor::SpawnOrReactivate(UWorld* World, TSubclassOf<class APooledActor> BulletClass, const FTransform& Transform, FVector BulletVelocity, AActor* BulletOwner, APawn* BulletInstigator)
+{
+	APooledActor* bullet;
+
+	APooledActor* Recycled = GetFromPool(World, BulletClass);
+
+	if (Recycled)
+	{
+		APooledActor* Default = Cast<APooledActor>(BulletClass->GetDefaultObject());
+
+		Recycled->SetOwner(BulletOwner);
+		Recycled->SetInstigator(BulletInstigator);
+		Recycled->SetActorTransform(Transform);
+		Recycled->Velocity = BulletVelocity;
+		Recycled->SetActorHiddenInGame(Default->IsHidden());
+		Recycled->SetActorTickEnabled(true);
+		Recycled->CanRetrace = false;
+		Recycled->IgnoredActors = Default->IgnoredActors;
+		Recycled->SafeDelay = Default->SafeDelay;
+		Recycled->SetLifeSpan(Default->InitialLifeSpan);
+		if (!Recycled->HasActorBegunPlay()) { Recycled->BeginPlay(); }
+		Recycled->ReactivationBroadcast(UGameplayStatics::RebaseLocalOriginOntoZero(Recycled->GetWorld(), Transform.GetLocation()), BulletVelocity, BulletOwner, BulletInstigator);
+#ifdef WITH_EDITOR
+		if (Recycled->DebugPooling)
+		{
+			GEngine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Recycling pooled bullet"));
+		}
+#endif
+		return Recycled;
+	}
+	else
+	{
+		bullet = Cast<APooledActor>(World->SpawnActorDeferred<APooledActor>(BulletClass, Transform, BulletOwner, BulletInstigator));
+		bullet->RandomStream.GenerateNewSeed();
+		bullet->Velocity = BulletVelocity;
+		UGameplayStatics::FinishSpawningActor(bullet, Transform);
+#ifdef WITH_EDITOR
+		if (bullet->DebugPooling)
+		{
+			GEngine->AddOnScreenDebugMessage(0, 2, FColor::Orange, TEXT("Spawning new bullet"));
+		}
+#endif
+		return bullet;
+	}
+}
+
+void APooledActor::ReactivationBroadcast_Implementation(FVector_NetQuantize NewLocation, FVector NewVelocity, AActor* BulletOwner, APawn* BulletInstigator)
+{
+	if (!HasAuthority())
+	{
+		APooledActor* Default = Cast<APooledActor>(this->StaticClass()->GetDefaultObject());
+
+		SetOwner(BulletOwner);
+		SetInstigator(BulletInstigator);
+
+		SetActorLocation(UGameplayStatics::RebaseZeroOriginOntoLocal(GetWorld(), NewLocation));
+		Velocity = NewVelocity;
+		CanRetrace = false;
+
+		SetActorHiddenInGame(Default->IsHidden());
+		SetActorTickEnabled(true);
+		SafeDelay = Default->SafeDelay;
+		OwnerSafe = Default->SafeLaunch;
+		BeginPlay();
+	}
+}
+
+void APooledActor::DeactivationBroadcast_Implementation()
+{
+	if (!HasAuthority())
+	{
+		this->DeativateToPool();
+	}
+}
+
+void APooledActor::LifeSpanExpired()
+{
+	Deactivate();
+}
+
+void APooledActor::DeativateToPool()
+{
+	APooledActor* Pool = Cast<APooledActor>(GetClass()->GetDefaultObject());
+
+	if (Pool && EnablePooling)
+	{
+		SetActorHiddenInGame(true);
+		SetActorTickEnabled(false);
+		Pool->Pooled.Add(this);
+		if (HasActorBegunPlay())
+		{
+			EndPlay(EEndPlayReason::RemovedFromWorld);
+		}
+
+		if (Pool->Pooled.Num() > MaxPoolSize)
+		{
+			APooledActor* Oldest = (Pool->Pooled[0].Get());
+			Pool->Pooled.RemoveAtSwap(0);
+			if (Oldest) { Oldest->Destroy(); }
+		}
+
+#ifdef WITH_EDITOR
+		if (DebugPooling)
+		{
+			GEngine->AddOnScreenDebugMessage(2, 2, FColor::White, FString("Bullet pooled: ") + FString::FromInt(Pool->Pooled.Num()));
+		}
+#endif
+	}
+	else
+	{
+		Destroy();
+	}
+}
---
+++ SonicShooter/Source/SonicShooter/Public/Actor/PooledActor.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PooledActor.generated.h"
+
+UCLASS()
+class SONICSHOOTER_API APooledActor : public AActor
+{
+	GENERATED_BODY()
+	
+
+public:
+	APooledActor();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Pooling")
+		bool EnablePooling = true;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Safe launch")
+		bool SafeLaunch = true;
+	UPROPERTY(BlueprintReadWrite, Category = "State")
+		bool OwnerSafe = false;
+	UPROPERTY(Replicated, BlueprintReadWrite, Category = "State")
+		FVector Velocity;
+	UPROPERTY(Replicated, BlueprintReadWrite, Category = "State")
+		FRandomStream RandomStream;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Collision")
+		TArray<AActor*> IgnoredActors;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Safe launch", Meta = (EditCondition = "SafeLaunch"))
+		float SafeDelay = 1.0;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Debug")
+		bool DebugPooling;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Pooling")
+		int MaxPoolSize = 50;
+
+	//pooling
+	UFUNCTION(BlueprintAuthorityOnly, BlueprintCallable, Category = "EBBullet|Pooling")
+		void Deactivate();
+	UFUNCTION(NetMulticast, Reliable)
+		void ReactivationBroadcast(FVector_NetQuantize NewLocation, FVector NewVelocity, AActor* BulletOwner, APawn* BulletInstigator);
+	UFUNCTION(NetMulticast, Reliable)
+		void DeactivationBroadcast();
+	virtual void LifeSpanExpired() override;
+private:
+	UPROPERTY()
+		TArray<TWeakObjectPtr<AActor>> Pooled;
+
+	bool CanRetrace = false;
+	static APooledActor* GetFromPool(UWorld* World, UClass* BulletClass);
+	static APooledActor* SpawnOrReactivate(UWorld* World, TSubclassOf<class APooledActor> BulletClass, const FTransform& Transform, FVector BulletVelocity, AActor* BulletOwner, APawn* BulletInstigator);
+	void DeativateToPool();
+};

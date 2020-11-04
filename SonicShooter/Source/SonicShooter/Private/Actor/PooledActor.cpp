8ee950bcb563363d0713107ae75aecab691593a2
Brian2524 <b2hinkle@gmail.com>
2020-11-04 18:55:47 -0500

    bCanAlwaysTick = true

--- SonicShooter/Source/SonicShooter/Private/Actor/PooledActor.cpp
+++ SonicShooter/Source/SonicShooter/Private/Actor/PooledActor.cpp
@@ -18,7 +18,7 @@ void APooledActor::GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLi
 APooledActor::APooledActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 

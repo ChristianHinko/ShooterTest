8707e916f135e3650cd248e12dd66f1ea8021739
Brian2524 <b2hinkle@gmail.com>
2020-11-05 21:33:25 -0500

    add crouch ability class

---
+++ SonicShooter/Source/SonicShooter/Public/Character/Abilities/GA_CharacterCrouch.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/SSGameplayAbility.h"
+#include "GA_CharacterCrouch.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SONICSHOOTER_API UGA_CharacterCrouch : public USSGameplayAbility
+{
+	GENERATED_BODY()
+	
+};

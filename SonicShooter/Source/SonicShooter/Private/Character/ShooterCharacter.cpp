d6082976f02c6acfabb433f15eaa355dcc5f7d04
Brian2524 <b2hinkle@gmail.com>
2020-12-09 17:37:08 -0500

    Disable camera sway system for now until we think of a final implementation for it

--- SonicShooter/Source/SonicShooter/Private/Character/ShooterCharacter.cpp
+++ SonicShooter/Source/SonicShooter/Private/Character/ShooterCharacter.cpp
@@ -44,60 +44,60 @@ bool AShooterCharacter::GrantStartingAbilities()
 	return true;
 }
 
-#include "Kismet/KismetSystemLibrary.h"
-#include "Kismet/KismetMathLibrary.h"
-#include "GameFramework/SpringArmComponent.h"
+//#include "Kismet/KismetSystemLibrary.h"
+//#include "Kismet/KismetMathLibrary.h"
+//#include "GameFramework/SpringArmComponent.h"
 void AShooterCharacter::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
 
-	float frameHorizontalMouseRate = 0;
-	float frameVerticalMouseRate = 0;
-	Cast<APlayerController>(GetController())->GetInputMouseDelta(frameHorizontalMouseRate, frameVerticalMouseRate);
-	
+	//float frameHorizontalMouseRate = 0;
+	//float frameVerticalMouseRate = 0;
+	//Cast<APlayerController>(GetController())->GetInputMouseDelta(frameHorizontalMouseRate, frameVerticalMouseRate);
+	//
 
-	// Weapon sway
-	if (POVMesh/* && AbilitySystem && !AbilitySystem->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("WeaponState.ADS")))*/)
-	{
-		FVector CameraSocketLocation = POVMesh->GetSocketTransform(TEXT("CameraBoneSocket"), ERelativeTransformSpace::RTS_ParentBoneSpace).GetLocation();
-		FVector FrameMouseRate = FVector(0, -frameHorizontalMouseRate, -frameVerticalMouseRate);
-		FVector NewCameraLocation = CameraSocketLocation - (FrameMouseRate * CameraSwayAmount);
-		if (GetCameraBoom())
-		{
-			FVector CurrentCameraLocation = FVector(GetCameraBoom()->GetRelativeTransform().GetLocation());
+	//// Weapon sway
+	//if (POVMesh/* && AbilitySystem && !AbilitySystem->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("WeaponState.ADS")))*/)
+	//{
+	//	FVector CameraSocketLocation = POVMesh->GetSocketTransform(TEXT("CameraBoneSocket"), ERelativeTransformSpace::RTS_ParentBoneSpace).GetLocation();
+	//	FVector FrameMouseRate = FVector(0, -frameHorizontalMouseRate, -frameVerticalMouseRate);
+	//	FVector NewCameraLocation = CameraSocketLocation - (FrameMouseRate * CameraSwayAmount);
+	//	if (GetCameraBoom())
+	//	{
+	//		FVector CurrentCameraLocation = FVector(GetCameraBoom()->GetRelativeTransform().GetLocation());
 
-			GetCameraBoom()->SetRelativeLocation(UKismetMathLibrary::VInterpTo(CurrentCameraLocation, NewCameraLocation, DeltaSeconds, 10));
-		}
+	//		GetCameraBoom()->SetRelativeLocation(UKismetMathLibrary::VInterpTo(CurrentCameraLocation, NewCameraLocation, DeltaSeconds, 10));
+	//	}
 
-		
-	}
-	else
-	{
-		FVector CameraSocketLocation = POVMesh->GetSocketTransform(TEXT("CameraBoneSocket"), ERelativeTransformSpace::RTS_ParentBoneSpace).GetLocation();
-		FVector NewCameraLocation = FVector(0, 0, 0);
-		if (GetCameraBoom())
-		{
-			FVector CurrentCameraLocation = FVector(GetCameraBoom()->GetRelativeTransform().GetLocation());
+	//	
+	//}
+	//else
+	//{
+	//	FVector CameraSocketLocation = POVMesh->GetSocketTransform(TEXT("CameraBoneSocket"), ERelativeTransformSpace::RTS_ParentBoneSpace).GetLocation();
+	//	FVector NewCameraLocation = FVector(0, 0, 0);
+	//	if (GetCameraBoom())
+	//	{
+	//		FVector CurrentCameraLocation = FVector(GetCameraBoom()->GetRelativeTransform().GetLocation());
 
-			float yaw = CurrentCameraLocation.Y - frameHorizontalMouseRate;
-			float pitch = CurrentCameraLocation.Z - frameVerticalMouseRate;
-			FVector Sway = FVector(CurrentCameraLocation.X, yaw, pitch);
+	//		float yaw = CurrentCameraLocation.Y - frameHorizontalMouseRate;
+	//		float pitch = CurrentCameraLocation.Z - frameVerticalMouseRate;
+	//		FVector Sway = FVector(CurrentCameraLocation.X, yaw, pitch);
 
-			FVector ADSCameraSwayAmount = CameraSwayAmount + AddedCameraSwayDuringADS;
+	//		FVector ADSCameraSwayAmount = CameraSwayAmount + AddedCameraSwayDuringADS;
 
-			NewCameraLocation = CameraSocketLocation - (Sway * ADSCameraSwayAmount);
-
-
-
-
-			GetCameraBoom()->SetRelativeLocation(UKismetMathLibrary::VInterpTo(CurrentCameraLocation, NewCameraLocation, DeltaSeconds, 10));
-		}
+	//		NewCameraLocation = CameraSocketLocation - (Sway * ADSCameraSwayAmount);
 
 
 
 
-	}
+	//		GetCameraBoom()->SetRelativeLocation(UKismetMathLibrary::VInterpTo(CurrentCameraLocation, NewCameraLocation, DeltaSeconds, 10));
+	//	}
+
+
+
+
+	//}
 }
 
 void AShooterCharacter::OnInteractPressed()

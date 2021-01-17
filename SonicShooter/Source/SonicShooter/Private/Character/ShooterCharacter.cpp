ChristianHinko/ShooterTest

commit afda62d953f355f877248e6df6c0ae766a133397

Create and start using out new SSArcInventory_Active component

SonicShooter/Source/SonicShooter/Private/Character/ShooterCharacter.cpp
@@ -8,7 +8,7 @@
 #include "AbilitySystemComponent.h"
 #include "ActorComponents/InteractorComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "Components/ArcInventoryComponent_Active.h"
+#include "Inventory/SSArcInventoryComponent_Active.h"
 
 
 
@@ -33,7 +33,7 @@ AShooterCharacter::AShooterCharacter(const FObjectInitializer& ObjectInitializer
 	: Super(ObjectInitializer.SetDefaultSubobjectClass<UArcInventoryComponent_Active>(InventoryComponentName))
 {
 	InventoryComponent = CreateDefaultSubobject<UArcInventoryComponent>(InventoryComponentName);
-	InventoryComponentActive = Cast<UArcInventoryComponent_Active>(InventoryComponent);
+	SSInventoryComponentActive = Cast<USSArcInventoryComponent_Active>(InventoryComponent);
 
 
 
@@ -67,7 +67,7 @@ bool AShooterCharacter::GrantStartingAbilities()
 	return true;
 }
 
-#include "Kismet/KismetSystemLibrary.h"
+//#include "Kismet/KismetSystemLibrary.h"
 //#include "Kismet/KismetMathLibrary.h"
 //#include "GameFramework/SpringArmComponent.h"
 void AShooterCharacter::Tick(float DeltaSeconds)

SonicShooter/SonicShooter.uproject
@@ -10,7 +10,8 @@
 			"LoadingPhase": "Default",
 			"AdditionalDependencies": [
 				"Engine",
-				"GameplayAbilities"
+				"GameplayAbilities",
+				"ArcInventory"
 			]
 		}
 	],

SonicShooter/Source/SonicShooter/Private/Inventory/GA_SwapActiveItem.cpp
@@ -5,7 +5,7 @@
 
 
 #include "Character/ShooterCharacter.h"
-#include "Components/ArcInventoryComponent_Active.h"
+#include "Inventory/SSArcInventoryComponent_Active.h"
 #include "SonicShooter/Private/Utilities/LogCategories.h"
 
 UGA_SwapActiveItem::UGA_SwapActiveItem()
@@ -39,10 +39,10 @@ void UGA_SwapActiveItem::OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo,
 		return;
 	}
 
-	InventoryComponentActive = ShooterCharacter->InventoryComponentActive;
-	if (!InventoryComponentActive)
+	SSInventoryComponentActive = ShooterCharacter->SSInventoryComponentActive;
+	if (!SSInventoryComponentActive)
 	{
-		UE_LOG(LogGameplayAbility, Error, TEXT("%s() InventoryComponentActive was NULL"), *FString(__FUNCTION__));
+		UE_LOG(LogGameplayAbility, Error, TEXT("%s() SSInventoryComponentActive was NULL"), *FString(__FUNCTION__));
 		return;
 	}
 }
@@ -61,9 +61,9 @@ bool UGA_SwapActiveItem::CanActivateAbility(const FGameplayAbilitySpecHandle Han
 		return false;
 	}
 
-	if (!InventoryComponentActive)
+	if (!SSInventoryComponentActive)
 	{
-		UE_LOG(LogGameplayAbility, Error, TEXT("%s() InventoryComponentActive was NULL"), *FString(__FUNCTION__));
+		UE_LOG(LogGameplayAbility, Error, TEXT("%s() SSInventoryComponentActive was NULL"), *FString(__FUNCTION__));
 		return false;
 	}
 
@@ -100,7 +100,7 @@ void UGA_SwapActiveItem::PerformSwap()
 				UE_LOG(LogGameplayAbility, Warning, TEXT("%s() Can't switch to item index below 0 (none exist)"), *FString(__FUNCTION__));
 				break;
 			}
-			InventoryComponentActive->SwapActiveItems(itemSlotIndexToSwitchTo);
+			SSInventoryComponentActive->SwapActiveItems(itemSlotIndexToSwitchTo);
 			break;
 		case ESwapMethod::ByTag:
 			if (ItemSlotTagToSwitchTo.IsValid() || ItemSlotTagToSwitchTo == FGameplayTag::EmptyTag)
@@ -108,13 +108,13 @@ void UGA_SwapActiveItem::PerformSwap()
 				UE_LOG(LogGameplayAbility, Warning, TEXT("%s() Tag not valid when trying to switch to inventory slot with the tag"), *FString(__FUNCTION__));
 				break;
 			}
-			InventoryComponentActive->SwapActiveItems(InventoryComponentActive->GetActiveItemIndexByTag(ItemSlotTagToSwitchTo));
+			SSInventoryComponentActive->SwapActiveItems(SSInventoryComponentActive->GetActiveItemIndexByTag(ItemSlotTagToSwitchTo));
 			break;
 		case ESwapMethod::NextItem:
-			InventoryComponentActive->SwapActiveItems(InventoryComponentActive->GetNextActiveItemSlot());
+			SSInventoryComponentActive->SwapActiveItems(SSInventoryComponentActive->GetNextActiveItemSlot());
 			break;
 		case ESwapMethod::PrecedingItem:
-			InventoryComponentActive->SwapActiveItems(InventoryComponentActive->GetPreviousActiveItemSlot());
+			SSInventoryComponentActive->SwapActiveItems(SSInventoryComponentActive->GetPreviousActiveItemSlot());
 			break;
 		case ESwapMethod::LastActiveItem:
 

SonicShooter/Source/SonicShooter/Private/Inventory/SSArcInventoryComponent_Active.cpp
@@ -1,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Inventory/SSArcInventoryComponent_Active.h"
+

SonicShooter/Source/SonicShooter/Public/Character/ShooterCharacter.h
@@ -11,7 +11,7 @@
 class IInteractable;
 class UInteractorComponent;
 class UArcInventoryComponent;
-class UArcInventoryComponent_Active;
+class USSArcInventoryComponent_Active;
 
 /**
  *
@@ -31,7 +31,7 @@ public:
 	AShooterCharacter(const FObjectInitializer& ObjectInitializer);
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Inventory")
-		class UArcInventoryComponent_Active* InventoryComponentActive;
+		USSArcInventoryComponent_Active* SSInventoryComponentActive;
 	/**
 	 * This setup, with the static FName InventoryComponentName and SetDefaultSubobjectClass in the constructor allows you to
 	 * have a bit of a heirarchy for your character classes. For example, you can have a base class that uses a basic

SonicShooter/Source/SonicShooter/Public/Inventory/GA_SwapActiveItem.h
@@ -7,7 +7,7 @@
 #include "GA_SwapActiveItem.generated.h"
 
 class AShooterCharacter;
-class UArcInventoryComponent_Active;
+class USSArcInventoryComponent_Active;
 
 /** How we want to be able to swap item */
 UENUM()
@@ -42,7 +42,7 @@ protected:
 	UPROPERTY()
 		AShooterCharacter* ShooterCharacter;
 	UPROPERTY()
-		UArcInventoryComponent_Active* InventoryComponentActive;
+		USSArcInventoryComponent_Active* SSInventoryComponentActive;
 
 
 

SonicShooter/Source/SonicShooter/Public/Inventory/SSArcInventoryComponent_Active.h
@@ -1,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ArcInventoryComponent_Active.h"
+#include "SSArcInventoryComponent_Active.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SONICSHOOTER_API USSArcInventoryComponent_Active : public UArcInventoryComponent_Active
+{
+	GENERATED_BODY()
+	
+};

commit b36169710d49503f7066a428761152ab200c3904

Fix a problem we started encountering since commit afda62d953f355f877248e6df6c0ae766a133397. Our character's inventory component was NULL after switching to using our subclassed version of ArcInventoryComponent_Active

SonicShooter/Source/SonicShooter/Private/Character/ShooterCharacter.cpp
@@ -32,7 +32,7 @@ void AShooterCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 FName AShooterCharacter::InventoryComponentName(TEXT("InventoryComponent"));
 
 AShooterCharacter::AShooterCharacter(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer.SetDefaultSubobjectClass<UArcInventoryComponent_Active>(InventoryComponentName))
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<USSArcInventoryComponent_Active>(InventoryComponentName))
 {
 	InventoryComponent = CreateDefaultSubobject<UArcInventoryComponent>(InventoryComponentName);
 	SSInventoryComponentActive = Cast<USSArcInventoryComponent_Active>(InventoryComponent);
@@ -71,12 +71,14 @@ bool AShooterCharacter::GrantStartingAbilities()
 	return true;
 }
 
-//#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
 //#include "Kismet/KismetMathLibrary.h"
 //#include "GameFramework/SpringArmComponent.h"
 void AShooterCharacter::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
+
+	SSInventoryComponentActive ? UKismetSystemLibrary::PrintString(this, "Valid") : UKismetSystemLibrary::PrintString(this, "NULLL");
 
 	//float frameHorizontalMouseRate = 0;
 	//float frameVerticalMouseRate = 0;
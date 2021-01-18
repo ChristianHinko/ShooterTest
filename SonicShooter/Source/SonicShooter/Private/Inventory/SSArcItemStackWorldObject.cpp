7306d6de72755ad595251b368f2f93f7c74f4aad
Brian2524 <b2hinkle@gmail.com>
2021-01-18 02:20:28 -0500

    Make SSArcItemStackWorldObject an empty implementation. Everything already handled by our parent class (nothing edited)

---
+++ SonicShooter/Source/SonicShooter/Private/Inventory/SSArcItemStackWorldObject.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Inventory/SSArcItemStackWorldObject.h"
+#include "Item\ArcItemDefinition_New.h"
+
+#include "ArcInventory.h"
+
---
+++ SonicShooter/Source/SonicShooter/Public/Inventory/SSArcItemStackWorldObject.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item\ArcItemStackWorldObject.h"
+
+#include "SSArcItemStackWorldObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SONICSHOOTER_API ASSArcItemStackWorldObject : public AArcItemStackWorldObject
+{
+	GENERATED_BODY()
+
+protected:
+
+};

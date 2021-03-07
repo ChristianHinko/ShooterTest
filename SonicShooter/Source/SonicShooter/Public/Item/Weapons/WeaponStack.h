c56daf9390b261478652acdc140d895de1d4a84b
Brian2524 <b2hinkle@gmail.com>
2021-03-06 20:19:30 -0500

    Create WeaponStack C++ class

---
+++ SonicShooter/Source/SonicShooter/Public/Item/Weapons/WeaponStack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ArcItemStack.h"
+#include "WeaponStack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SONICSHOOTER_API UWeaponStack : public UArcItemStack
+{
+	GENERATED_BODY()
+	
+};

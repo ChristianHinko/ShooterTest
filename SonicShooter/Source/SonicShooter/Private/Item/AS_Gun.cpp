2617a930429a591dd08972db92e94946382f3bf6
include-iostream <c4hinkle@gmail.com>
2021-03-14 15:41:45 -0300

    make AS_Gun and have all of the item definitions use it

---
+++ SonicShooter/Source/SonicShooter/Private/Item/AS_Gun.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/AS_Gun.h"
+
+#include "Net/UnrealNetwork.h"
+
+
+
+void UAS_Gun::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAS_Gun, MinBulletSpread, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAS_Gun, MaxBulletSpread, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAS_Gun, BulletSpreadIncPerShot, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAS_Gun, BulletSpreadDecRate, COND_None, REPNOTIFY_Always);
+}
+
+UAS_Gun::UAS_Gun()
+	: MinBulletSpread(0.f),
+	MaxBulletSpread(10.f),
+	BulletSpreadIncPerShot(1.f),
+	BulletSpreadDecRate(10.f)
+{
+	SetSoftAttributeDefaults();
+
+
+
+
+}
+
+void UAS_Gun::SetSoftAttributeDefaults()
+{
+	Super::SetSoftAttributeDefaults();
+
+
+	CurrentBulletSpread = GetMinBulletSpread();
+}
+
+
+
+
+// OnReps:
+
+
+void UAS_Gun::OnRep_MinBulletSpread(const FGameplayAttributeData& ServerBaseValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAS_Gun, MinBulletSpread, ServerBaseValue);
+}
+
+void UAS_Gun::OnRep_MaxBulletSpread(const FGameplayAttributeData& ServerBaseValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAS_Gun, MaxBulletSpread, ServerBaseValue);
+}
+
+void UAS_Gun::OnRep_BulletSpreadIncPerShot(const FGameplayAttributeData& ServerBaseValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAS_Gun, BulletSpreadIncPerShot, ServerBaseValue);
+}
+
+void UAS_Gun::OnRep_BulletSpreadDecRate(const FGameplayAttributeData& ServerBaseValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAS_Gun, BulletSpreadDecRate, ServerBaseValue);
+}

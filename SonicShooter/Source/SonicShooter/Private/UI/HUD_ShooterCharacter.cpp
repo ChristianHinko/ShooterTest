cd09a6a639fadb25c2d1e885c9cee4aa16f5b766
include-iostream <c4hinkle@gmail.com>
2021-03-17 20:10:20 -0300

    make HUD_ShooterCharacter and make it add the UW_Crosshair to the viewport. Make this hud class the default in the gamemode

---
+++ SonicShooter/Source/SonicShooter/Private/UI/HUD_ShooterCharacter.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/HUD_ShooterCharacter.h"
+
+#include "Blueprint/WidgetBlueprintLibrary.h"
+
+
+
+AHUD_ShooterCharacter::AHUD_ShooterCharacter()
+{
+
+}
+void AHUD_ShooterCharacter::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+
+}
+
+void AHUD_ShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CrosshairWidget = UWidgetBlueprintLibrary::Create(this, CrosshairWidgetTSub, GetOwningPlayerController());
+	CrosshairWidget->AddToViewport();
+}

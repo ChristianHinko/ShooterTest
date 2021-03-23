7492a538124ba449e3fabde4d2bb5f0271264838
Brian2524 <b2hinkle@gmail.com>
2021-03-23 00:09:33 -0400

    Implement bare bones GEEC_GunDealDamage.cpp. Very basic but it should work.

--- SonicShooter/Source/SonicShooter/Private/Item/Weapons/GEEC_GunDealDamage.cpp
+++ SonicShooter/Source/SonicShooter/Private/Item/Weapons/GEEC_GunDealDamage.cpp
@@ -5,20 +5,27 @@
 
 #include "AbilitySystem/SSAbilitySystemComponent.h"
 #include "AbilitySystem\AttributeSets\AS_Health.h"
-#include "Item/AS_Gun.h"
+#include "AbilitySystem/AttributeSets/AS_Damage.h"
 
 
 // Declare the attributes to capture and define how we want to capture them from the Source and Target.
 struct SSDamageStatics
 {
+	// Source
+	DECLARE_ATTRIBUTE_CAPTUREDEF(OutgoingDamage);
+	
+	// Target
 	DECLARE_ATTRIBUTE_CAPTUREDEF(IncomingDamage);
+
+
+
 
 	SSDamageStatics()
 	{
 		// Snapshot happens at time of GESpec creation
 
 		//Source captures
-		//DEFINE_ATTRIBUTE_CAPTUREDEF(UAS_Gun, Damage, Source, true);	// This will be the value to damage the target by
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAS_Damage, OutgoingDamage, Source, true);	// This will be the value to damage the target by
 
 		//Target captures
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAS_Health, IncomingDamage, Target, false);	// This is the attribute we will change on the target
@@ -33,35 +40,35 @@ static const SSDamageStatics& DamageStatics()
 
 UGEEC_GunDealDamage::UGEEC_GunDealDamage()
 {
+	//Source
+	RelevantAttributesToCapture.Add(DamageStatics().OutgoingDamageDef);
+
+	//Target
 	RelevantAttributesToCapture.Add(DamageStatics().IncomingDamageDef);
 }
 
-// Need to make this get the damage attribute from a damage attribute set from the source
+// Need to make this get the OutgoingDamage attribute from a damage attribute set on the source
 void UGEEC_GunDealDamage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, OUT FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
 {
-	//UAbilitySystemComponent* TargetAbilitySystemComponent = ExecutionParams.GetTargetAbilitySystemComponent();
-	//UAbilitySystemComponent* SourceAbilitySystemComponent = ExecutionParams.GetSourceAbilitySystemComponent();
+	UAbilitySystemComponent* TargetAbilitySystemComponent = ExecutionParams.GetTargetAbilitySystemComponent();
+	UAbilitySystemComponent* SourceAbilitySystemComponent = ExecutionParams.GetSourceAbilitySystemComponent();
 
-	//if (!SourceAbilitySystemComponent || !TargetAbilitySystemComponent)
-	//{
-	//	return;
-	//}
+	if (!SourceAbilitySystemComponent || !TargetAbilitySystemComponent)
+	{
+		return;
+	}
 
 
-	//AActor* SourceActor = SourceAbilitySystemComponent ? SourceAbilitySystemComponent->GetAvatarActor() : nullptr;
-	//AActor* TargetActor = TargetAbilitySystemComponent ? TargetAbilitySystemComponent->GetAvatarActor() : nullptr;
+	AActor* SourceActor = SourceAbilitySystemComponent ? SourceAbilitySystemComponent->GetAvatarActor() : nullptr;
+	AActor* TargetActor = TargetAbilitySystemComponent ? TargetAbilitySystemComponent->GetAvatarActor() : nullptr;
 
-	//const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
-	//FGameplayTagContainer AssetTags;
-	//Spec.GetAllAssetTags(AssetTags);
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+	FGameplayTagContainer AssetTags;
+	Spec.GetAllAssetTags(AssetTags);
 
-	//// Gather the tags from the source and target as that can affect how the damage might be calulated
-	//const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
-	//const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
-
-	//FAggregatorEvaluateParameters EvaluationParameters;
-	//EvaluationParameters.SourceTags = SourceTags;
-	//EvaluationParameters.TargetTags = TargetTags;
+	// Gather the tags from the source and target as that can affect how the damage might be calulated
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
 
 
 
@@ -69,14 +76,20 @@ void UGEEC_GunDealDamage::Execute_Implementation(const FGameplayEffectCustomExec
 
 
 
-	//
-	//float RawDamage = 0.0f;		// Raw because we havn't done any post process stuff yet (ie. crit multiplier, enemy armor)
 
-	//// Capture optional damage value set on the damage GE as a CalculationModifier under the ExecutionCalculation
-	//ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().IncomingDamageDef, EvaluationParameters, RawDamage);
 
-	//// Set the Target's damage meta attribute
-	//OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(DamageStatics().DamageProperty, EGameplayModOp::Additive, RawDamage));
+
+
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+	float RawDamage = 0.0f;		// Raw because we havn't done any post process stuff yet (ie. crit multiplier, enemy armor)
+	// Capture optional damage value set on the damage GE as a CalculationModifier under the ExecutionCalculation
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().OutgoingDamageDef, EvaluationParameters, RawDamage);
+
+	// Set the Target's IncomingDamage meta attribute
+	OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(DamageStatics().IncomingDamageProperty, EGameplayModOp::Additive, RawDamage));
 
 
 

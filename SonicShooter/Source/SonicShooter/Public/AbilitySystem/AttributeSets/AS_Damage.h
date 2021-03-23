6b672cc418bfa433758d03ed247f3c1efcce1715
Brian2524 <b2hinkle@gmail.com>
2021-03-22 21:25:20 -0400

    Create AS_Damage for a generic attribute set

---
+++ SonicShooter/Source/SonicShooter/Public/AbilitySystem/AttributeSets/AS_Damage.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/SSAttributeSet.h"
+#include "AS_Damage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SONICSHOOTER_API UAS_Damage : public USSAttributeSet
+{
+	GENERATED_BODY()
+	
+};

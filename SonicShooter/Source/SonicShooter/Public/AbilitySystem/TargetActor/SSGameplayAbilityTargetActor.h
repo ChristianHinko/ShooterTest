8d5804586a929fbcdbc924f8a99eeedc65a52ddb
include-iostream <c4hinkle@gmail.com>
2021-04-11 22:51:50 -0300

    move aiming code in base target actor class and clean up both GATAs

--- SonicShooter/Source/SonicShooter/Public/AbilitySystem/TargetActor/SSGameplayAbilityTargetActor.h
+++ SonicShooter/Source/SonicShooter/Public/AbilitySystem/TargetActor/SSGameplayAbilityTargetActor.h
@@ -11,11 +11,9 @@
 
 
 /**
- * Base target actor class.
- * 
- * Provides filtering functionality
+ * Base target actor class
  */
-UCLASS()
+UCLASS(Abstract, notplaceable)
 class SONICSHOOTER_API ASSGameplayAbilityTargetActor : public AGameplayAbilityTargetActor
 {
 	GENERATED_BODY()
@@ -24,20 +22,44 @@ public:
 	ASSGameplayAbilityTargetActor(const FObjectInitializer& ObjectInitializer);
 
 
-
-	/** Filter out hit results that do not pass filter and removes multiple hits per actor if needed */
-	void FilterHitResults(TArray<FHitResult>& OutHitResults, const FGATDF_MultiFilterHandle FilterHandle, const bool inAllowMultipleHitsPerActor) const;
-
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ExposeOnSpawn = true), Category = "Trace")
+	/** Our custom gameplay ability target data filter */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ExposeOnSpawn = true), Category = "Target Data")
 		FGATDF_MultiFilter MultiFilter;
 	FGATDF_MultiFilterHandle MultiFilterHandle;
 
 	/**
 	 * If true, when a trace overlaps an actor's multiple collisions, those multiple collision hits will add
 	 * that actor to the hitresults multiple times.
+	 * 
+	 * TODO: should not apply to multiple hits from multiple ricochets
 	 */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ExposeOnSpawn = true), Category = "Trace")
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ExposeOnSpawn = true), Category = "Target Data")
 		bool bAllowMultipleHitsPerActor;
+
+
+	/** Filter out hit results that do not pass filter and removes multiple hits per actor if needed */
+	void FilterHitResults(TArray<FHitResult>& OutHitResults, const FGATDF_MultiFilterHandle FilterHandle, const bool inAllowMultipleHitsPerActor) const;
+
+
+
+
+	/** Max range of this target actor (not required for all target actors) */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ExposeOnSpawn = true), Category = "Trace")
+		float MaxRange;
+
+	/** Trace channel for this target actor (not required for all target actors) */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ExposeOnSpawn = true), Category = "Trace")
+		TEnumAsByte<ECollisionChannel> TraceChannel;
+
+	/** Does the trace affect the aiming pitch */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ExposeOnSpawn = true), Category = "Trace")
+		bool bTraceAffectsAimPitch;
+
+	/** Outputs a point that the player controller is looking at (within the MaxRange) (also this uses TraceChannel) */
+	void AimWithPlayerController(const AActor* InSourceActor, FCollisionQueryParams Params, const FVector& TraceStart, FVector& OutTraceEnd) const;
+	/** Outputs a direction to use rather than a trace endpoint */
+	void DirWithPlayerController(const AActor* InSourceActor, FCollisionQueryParams Params, const FVector& TraceStart, FVector& OutTraceDir) const;
+
+	static bool ClipCameraRayToAbilityRange(FVector CameraLocation, FVector CameraDirection, FVector AbilityCenter, float AbilityRange, FVector& ClippedPosition);
 
 };

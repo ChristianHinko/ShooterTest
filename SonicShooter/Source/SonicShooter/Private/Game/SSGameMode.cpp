b4a1ded8618a8a0e202f75a491b6aad76f5521ab
Brian2524 <b2hinkle@gmail.com>
2021-04-15 04:37:34 -0400

    Code cleanup/Comments

--- SonicShooter/Source/SonicShooter/Private/Game/SSGameMode.cpp
+++ SonicShooter/Source/SonicShooter/Private/Game/SSGameMode.cpp
@@ -52,10 +52,10 @@ APawn* ASSGameMode::SpawnDefaultPawnAtTransform_Implementation(AController* NewP
 
 void ASSGameMode::GiveInventoryStartupItems(UArcInventoryComponent* Inventory)
 {
+	USSArcInventoryComponent_Active* SSArcInventoryCompActive = Cast<USSArcInventoryComponent_Active>(Inventory);	// See if we are a USSArcInventoryComponent_Active so we can do extra logic on that specific type
+
 	// Loop through the SlotDefinitions we filled out in the inventory component's BP for this pawn so we can access the information of what item we should put in each slot
 	// We loop through them backwards since placing an active item into a slot will call USSArcInventoryComponent_Active::AddToActiveItemHistory. We want to add to that array backwards, so we get the item at index 0 as our most recent item.
-	USSArcInventoryComponent_Active* SSArcInventoryCompActive = Cast<USSArcInventoryComponent_Active>(Inventory);	// See if we are a USSArcInventoryComponent_Active
-
 	for (int32 i = Inventory->CustomInventorySlots.Num() - 1; i >= 0 ; --i)
 	{
 		//	Get the item generator the SlotDefinition specifies
@@ -71,6 +71,8 @@ void ASSGameMode::GiveInventoryStartupItems(UArcInventoryComponent* Inventory)
 				if (Inventory->GetSlotReferenceByIndex(i, SlotToAddItemTo))
 				{
 					Inventory->PlaceItemIntoSlot(GeneratedItem, SlotToAddItemTo);
+
+					// Now we want to fill our active item history array if we have it
 					if (SSArcInventoryCompActive)
 					{
 						SSArcInventoryCompActive->AddToActiveItemHistory(SlotToAddItemTo);

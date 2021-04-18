6b72ff29b51df60c15113dd1ac6ab4893ba05ff0
Brian2524 <b2hinkle@gmail.com>
2021-04-18 07:52:37 -0400

    modify coment

--- SonicShooter/Source/SonicShooter/Private/Game/SSGameMode.cpp
+++ SonicShooter/Source/SonicShooter/Private/Game/SSGameMode.cpp
@@ -55,7 +55,7 @@ void ASSGameMode::GiveInventoryStartupItems(UArcInventoryComponent* Inventory)
 	USSArcInventoryComponent_Active* SSArcInventoryCompActive = Cast<USSArcInventoryComponent_Active>(Inventory);	// See if we are a USSArcInventoryComponent_Active so we can do extra logic on that specific type
 
 	// Loop through the SlotDefinitions we filled out in the inventory component's BP for this pawn so we can access the information of what item we should put in each slot
-	// We loop through them backwards since we call USSArcInventoryComponent_Active::AddToActiveItemHistory after every equip
+	// We loop through them backwards since we call USSArcInventoryComponent_Active::AddToActiveItemHistory every iteration
 	for (int32 i = Inventory->CustomInventorySlots.Num() - 1; i >= 0 ; --i)
 	{
 		//	Get the item generator the SlotDefinition specifies

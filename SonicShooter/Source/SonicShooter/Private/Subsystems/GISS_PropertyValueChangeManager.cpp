8d8928f85d8d2aa3044a7df9dee092efb1e32abd
include-iostream <c4hinkle@gmail.com>
2021-05-03 08:40:56 -0300

    make the FFloatValueProperty struct and have it use the UGISS_PropertyValueChangeManager subsystem. (currently in a broken state - exception when opening editor)

--- SonicShooter/Source/SonicShooter/Private/Subsystems/GISS_PropertyValueChangeManager.cpp
+++ SonicShooter/Source/SonicShooter/Private/Subsystems/GISS_PropertyValueChangeManager.cpp
@@ -3,6 +3,48 @@
 
 #include "Subsystems/GISS_PropertyValueChangeManager.h"
 
+#include "Kismet/GameplayStatics.h"
+
+
+
+FFloatValueProperty::FFloatValueProperty(UObject* InOwner)
+{
+	Owner = InOwner;
+	ChangeManager = UGameplayStatics::GetGameInstance(Owner)->GetSubsystem<UGISS_PropertyValueChangeManager>();
+	Id = (ChangeManager->NextPropertyId)++;
+
+	ChangeManager->FloatValueChangeDelegates.Add(*this);
+}
+FFloatValueProperty::~FFloatValueProperty() // TODO: this gets hit and breaks on engine startup
+{
+	if (ChangeManager)
+	{
+		ChangeManager->FloatValueChangeDelegates.Remove(*this);
+	}
+}
+
+
+void FFloatValueProperty::SetValue(float NewValue)
+{
+	const float OldValue = Value;
+
+	Value = NewValue;
+
+	if (FFloatValueChange* ChangeDelegate = ChangeManager->FloatValueChangeDelegates.Find(*this))
+	{
+		ChangeDelegate->Broadcast(OldValue, NewValue);
+	}
+}
+
+double FFloatValueProperty::GetValue()
+{
+	return Value;
+}
+
+
+
+
+
 UGISS_PropertyValueChangeManager::UGISS_PropertyValueChangeManager()
 {
 
@@ -10,5 +52,28 @@ UGISS_PropertyValueChangeManager::UGISS_PropertyValueChangeManager()
 
 bool UGISS_PropertyValueChangeManager::ShouldCreateSubsystem(UObject* Outer) const
 {
-	return true;
+	return Super::ShouldCreateSubsystem(Outer);
+}
+
+
+void UGISS_PropertyValueChangeManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+
+}
+
+void UGISS_PropertyValueChangeManager::Deinitialize()
+{
+	Super::Deinitialize();
+
+
+	FloatValueChangeDelegates.Empty();
+}
+
+
+
+FFloatValueChange& UGISS_PropertyValueChangeManager::GetFloatValueChangeDelegate(const FFloatValueProperty& Property)
+{
+	return FloatValueChangeDelegates.FindOrAdd(Property);
 }
--- SonicShooter/Source/SonicShooter/Public/Subsystems/GISS_PropertyValueChangeManager.h
+++ SonicShooter/Source/SonicShooter/Public/Subsystems/GISS_PropertyValueChangeManager.h
@@ -4,7 +4,75 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/GameInstanceSubsystem.h"
+
 #include "GISS_PropertyValueChangeManager.generated.h"
+
+
+class UGISS_PropertyValueChangeManager;
+
+
+
+DECLARE_MULTICAST_DELEGATE_TwoParams(FFloatValueChange, const float& OldValue, const float& NewValue);
+
+
+USTRUCT()
+struct SONICSHOOTER_API FFloatValueProperty
+{
+	GENERATED_BODY()
+
+
+public:
+	FFloatValueProperty()
+	{
+
+	}
+	FFloatValueProperty(UObject* InOwner);
+
+
+	~FFloatValueProperty();
+
+
+	void SetValue(float NewValue);
+
+	double GetValue();
+
+	//BEGIN TMap requirements
+	friend uint32 GetTypeHash(const FFloatValueProperty& InValueProperty)
+	{
+		return InValueProperty.Id;
+	}
+
+	/** Equality/Inequality operators */
+	bool operator==(const FFloatValueProperty& Other) const
+	{
+		return ((Other.Id == Id));
+	}
+	bool operator!=(const FFloatValueProperty& Other) const
+	{
+		return ((Other.Id != Id));
+	}
+	//END TMap requirements
+
+	float operator=(const float& NewValue)
+	{
+		SetValue(NewValue);
+		return Value;
+	}
+
+	bool operator==(const float& Other) const
+	{
+		return (Value == Other);
+	}
+
+private:
+	UObject* Owner;
+
+	float Value;
+
+	int32 Id;
+	UGISS_PropertyValueChangeManager* ChangeManager;
+};
+
 
 /**
  * 
@@ -14,8 +82,25 @@ class SONICSHOOTER_API UGISS_PropertyValueChangeManager : public UGameInstanceSu
 {
 	GENERATED_BODY()
 
+private:
+	friend struct FFloatValueProperty;
+
 public:
 	UGISS_PropertyValueChangeManager();
 
 	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	virtual void Deinitialize() override;
+
+
+
+	FFloatValueChange& GetFloatValueChangeDelegate(const FFloatValueProperty& Property);
+
+private:
+	TMap<FFloatValueProperty, FFloatValueChange> FloatValueChangeDelegates;
+
+	int32 NextPropertyId;
 };

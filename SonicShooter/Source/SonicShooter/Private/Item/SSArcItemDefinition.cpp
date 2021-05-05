e83cd2660a2fdb8de42ba6fb029b6aa645dad9b5
Brian2524 <b2hinkle@gmail.com>
2021-05-05 16:12:54 -0400

    Rename some classes. We decided on trying to make the UI data stuff generic enough to work for any weapon. So this was the first step in doing that

---
+++ SonicShooter/Source/SonicShooter/Private/Item/SSArcItemDefinition.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/SSArcItemDefinition.h"
+
+
+
+USSUArcUIData_ItemDefinition::USSUArcUIData_ItemDefinition()
+{
+
+}
+
+USSArcItemDefinition::USSArcItemDefinition()
+{
+
+}

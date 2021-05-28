644cc01f7faa1e34fcf86dbe1ee87f21396777e7
Brian2524 <b2hinkle@gmail.com>
2021-05-28 05:21:19 -0400

    Add our new shooter game's phys material

---
+++ SonicShooter/Source/SonicShooter/Public/PhysicalMaterial/ShooterPhysicalMaterial.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
+#include "ShooterPhysicalMaterial.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable, CollapseCategories, HideCategories = Object)
+class SONICSHOOTER_API UShooterPhysicalMaterial : public UPhysicalMaterial
+{
+	GENERATED_BODY()
+
+public:
+	UShooterPhysicalMaterial();
+
+	/** This affects the bullet's speed when penetrating this physical material  */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Shooter)
+		float BulletPenetrationSpeedMultiplier;
+};

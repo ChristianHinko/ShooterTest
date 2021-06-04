89d66014c9af0fbc4a28eabe4e23961201e6fc03
Brian2524 <b2hinkle@gmail.com>
2021-06-04 06:59:42 -0400

    Function comment

--- SonicShooter/Source/SonicShooter/Public/Utilities/BlueprintFunctionLibraries/BFL_MathHelpers.h
+++ SonicShooter/Source/SonicShooter/Public/Utilities/BlueprintFunctionLibraries/BFL_MathHelpers.h
@@ -15,6 +15,9 @@ class SONICSHOOTER_API UBFL_MathHelpers : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 	
 public:
+	/**
+	 * @brief: Given a segment and a point, does that point lie on that segment?
+	*/
 	UFUNCTION(BlueprintPure, Category = "MathHelpers|VectorMath")
 		static bool PointLiesOnSegment(const FVector& Start, const FVector& End, const FVector& Point);
 };

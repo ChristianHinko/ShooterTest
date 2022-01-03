6186f26b79e9e510db30a98e67f00c63b61889ec
Christian Hinkle <c4hinkle@gmail.com>
2022-01-03 01:38:27 -0500

    fix our Native Gameplay Tags for UE 4.27 by getting rid of the UE_MODULE_NAME and UE_PLUGIN_NAME

--- SonicShooter/Source/SonicShooter/Private/Utilities/GameplayTags.cpp
+++ SonicShooter/Source/SonicShooter/Private/Utilities/GameplayTags.cpp
@@ -4,9 +4,6 @@
 
 
 
-#define UE_MODULE_NAME TEXT("SonicShooter")
-#define UE_PLUGIN_NAME TEXT("")
-
 UE_DEFINE_GAMEPLAY_TAG(JumpAbilityTag, TEXT("Ability.Movement.Jump"))
 UE_DEFINE_GAMEPLAY_TAG(RunAbilityTag, TEXT("Ability.Movement.Run"))
 UE_DEFINE_GAMEPLAY_TAG(CrouchAbilityTag, TEXT("Ability.Movement.Crouch"))

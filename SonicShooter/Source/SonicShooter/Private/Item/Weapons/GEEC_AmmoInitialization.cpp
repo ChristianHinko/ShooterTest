df4ad968bb4b9d369ea79de2ec65d77525323ada
Christian Hinkle <c4hinkle@gmail.com>
2022-01-21 05:36:43 -0500

    make GEEC_AmmoInitialization designed for initializing Attribute value defaults for AS_Ammo

---
+++ SonicShooter/Source/SonicShooter/Private/Item/Weapons/GEEC_AmmoInitialization.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/Weapons/GEEC_AmmoInitialization.h"
+
+#include "Item/AS_Ammo.h"
+
+
+
+struct FAmmoInitializationStatics
+{
+	// No need to capture these Attributes since we aren't reading from them, but writing to them
+	const FGameplayAttribute MaxAmmoAttribute;
+	const FGameplayAttribute MaxClipAmmoAttribute;
+	const FGameplayAttribute BackupAmmoAttribute;
+	
+	FAmmoInitializationStatics()
+		: MaxAmmoAttribute(UAS_Ammo::GetMaxAmmoAttribute())
+		, MaxClipAmmoAttribute(UAS_Ammo::GetMaxClipAmmoAttribute())
+		, BackupAmmoAttribute(UAS_Ammo::GetBackupAmmoAttribute())
+	{
+
+	}
+};
+
+static const FAmmoInitializationStatics& GetAmmoInitializationStatics()
+{
+	static FAmmoInitializationStatics Statics;
+	return Statics;
+}
+
+
+
+
+UGEEC_AmmoInitialization::UGEEC_AmmoInitialization()
+{
+	const UAS_Ammo* DefaultAmmoAttributeSet = GetDefault<UAS_Ammo>(UAS_Ammo::StaticClass());
+
+	MaxAmmo = DefaultAmmoAttributeSet->GetMaxAmmo();
+	MaxClipAmmo = DefaultAmmoAttributeSet->GetMaxClipAmmo();
+}
+
+void UGEEC_AmmoInitialization::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, OUT FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	Super::Execute_Implementation(ExecutionParams, OutExecutionOutput);
+
+	UAbilitySystemComponent* TargetAbilitySystemComponent = ExecutionParams.GetTargetAbilitySystemComponent();
+	if (!IsValid(TargetAbilitySystemComponent))
+	{
+		return;
+	}
+
+
+	// Set defaults
+	OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(GetAmmoInitializationStatics().MaxAmmoAttribute, EGameplayModOp::Override, MaxAmmo));
+	OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(GetAmmoInitializationStatics().MaxClipAmmoAttribute, EGameplayModOp::Override, MaxClipAmmo));
+
+	// Calculate defaults
+	float ClipAmmo = MaxClipAmmo;
+	float BackupAmmo = MaxAmmo - MaxClipAmmo;
+
+	OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(GetAmmoInitializationStatics().BackupAmmoAttribute, EGameplayModOp::Override, BackupAmmo));
+	
+	// Search for ammo Attribute Set and initialize ClipAmmo. TODO: this is really bad accessing an Ability System Component's Attribute Sets directly - move ClipAmmo somewhere else!
+	{
+		UAS_Ammo** AmmoAttributeSetPtr = nullptr;
+		int32* index = nullptr;
+		TargetAbilitySystemComponent->GetSpawnedAttributes_Mutable().FindItemByClass<UAS_Ammo>(AmmoAttributeSetPtr, index, 0);
+		if (AmmoAttributeSetPtr)
+		{
+			UAS_Ammo* AmmoAttributeSet = *AmmoAttributeSetPtr;
+			if (IsValid(AmmoAttributeSet))
+			{
+				AmmoAttributeSet->ClipAmmo = ClipAmmo;
+			}
+		}
+	}
+
+}
---
+++ SonicShooter/Source/SonicShooter/Public/Item/Weapons/GEEC_AmmoInitialization.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+
+#include "GEEC_AmmoInitialization.generated.h"
+
+
+
+/**
+ * Attribute Set default values for UAS_Ammo.
+ * Children can subclass this (in BP or cpp) and fill out default values
+ */
+UCLASS()
+class SONICSHOOTER_API UGEEC_AmmoInitialization : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UGEEC_AmmoInitialization();
+
+protected:
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, OUT FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+
+
+	UPROPERTY(EditDefaultsOnly, Category = "Defaults")
+		float MaxAmmo;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Defaults")
+		float MaxClipAmmo;
+
+};

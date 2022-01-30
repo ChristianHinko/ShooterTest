e3552f15c63b09811b46f5dfad1055fc7a25c1f4
Christian Hinkle <c4hinkle@gmail.com>
2022-01-30 16:57:15 -0500

    make O_Gun subobject with CurrentBulletSpread

---
+++ SonicShooter/Source/SonicShooter/Private/Subobjects/O_Gun.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subobjects/O_Gun.h"
+
+#include "Net/UnrealNetwork.h"
+
+
+
+void UO_Gun::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+
+	FDoRepLifetimeParams Params;
+	Params.Condition = COND_None;
+	Params.RepNotifyCondition = REPNOTIFY_OnChanged;
+	Params.bIsPushBased = true;
+
+	DOREPLIFETIME_WITH_PARAMS_FAST(UO_Gun, CurrentBulletSpread, Params);
+
+}
+
+UO_Gun::UO_Gun(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+	, OnCurrentBulletSpreadChange(TSharedRef<FFloatValueChange>())
+	, CurrentBulletSpread(this, FName("CurrentBulletSpread"), OnCurrentBulletSpreadChange)
+{
+	CurrentBulletSpread = 0.f;
+
+}
+
+
+bool UO_Gun::IsTickable() const
+{
+	return false;
+}
+void UO_Gun::Tick(float DeltaTime)
+{
+
+}
---
+++ SonicShooter/Source/SonicShooter/Public/Subobjects/O_Gun.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Wrappers/PropertyWrappers.h"
+
+#include "O_Gun.generated.h"
+
+
+
+/**
+ * Has CurrentBulletSpread float
+ */
+UCLASS()
+class SONICSHOOTER_API UO_Gun : public UObject, public FTickableGameObject
+{
+	GENERATED_BODY()
+
+public:
+		UO_Gun(const FObjectInitializer& ObjectInitializer);
+
+
+		TSharedRef<FFloatValueChange> OnCurrentBulletSpreadChange;
+
+		UPROPERTY(BlueprintReadOnly, Replicated, Category = "Gun")
+			FFloatPropertyWrapper CurrentBulletSpread;
+
+protected:
+		//BEGIN FTickableObjectBase interface
+		virtual bool IsTickable() const override;
+		virtual void Tick(float DeltaTime) override;
+		virtual TStatId GetStatId() const override { return TStatId(); }
+		//END FTickableObjectBase interface
+
+		//BEGIN FTickableGameObject interface
+		virtual bool IsTickableWhenPaused() const override { return false; };
+		//END FTickableGameObject interface
+
+};

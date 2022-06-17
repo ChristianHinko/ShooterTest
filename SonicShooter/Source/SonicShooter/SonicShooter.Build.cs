dd74702455f01bc9bee83919059780bdb63bf024
Christian Hinkle <c4hinkle@gmail.com>
2022-06-17 11:35:44 -0400

    Revert "Add new to-be-developed "AnimationSetup" plugin as a submodule"
    This reverts commit b3693e0bc515a9ae89cd884d45536dcf92250650.

--- SonicShooter/Source/SonicShooter/SonicShooter.Build.cs
+++ SonicShooter/Source/SonicShooter/SonicShooter.Build.cs
@@ -24,7 +24,6 @@ public class SonicShooter : ModuleRules
 				"AbilitySystemSetupExtras",
 				"HelperLibraries",
 				"PropertyWrapper",
-				"AnimationSetup",
 				"AutoSettings",
 				"ArcInventory",
 				"AnimGraphRuntime" // for KismetAnimationLibrary
